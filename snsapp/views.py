82b5bb009649d6da5ea17600b4da0448dbd0945e
kinno <kinnoyamasiro@gmail.com>
2023-09-26 00:32:17 +0900

    s

--- snsapp/views.py
+++ snsapp/views.py
@@ -8,8 +8,14 @@ from .models import Tag
 from .models import Post 
 from .forms import PostForm
 from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
-
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/login.html'
 
 class TagListView(ListView):
     """タグ一覧"""
