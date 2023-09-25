926124a00750239538839e5746525f922b44ed8a
kinno <kinnoyamasiro@gmail.com>
2023-09-26 03:23:40 +0900

    a

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth')
+            os.path.join(BASE_DIR, 'templates', 'account')
     ],
         'APP_DIRS': True,
         'OPTIONS': {
